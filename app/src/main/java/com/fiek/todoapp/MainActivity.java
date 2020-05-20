6cd215c4f95437cfcc6b5ad747edb8ae63bb9e55
MorinaElsa <morinaelsa01@gmail.com>
2020-05-20 23:18:26 +0200

    Krijimi i LinearLayout-it kryesor

--- app/src/main/java/com/fiek/todoapp/MainActivity.java
+++ app/src/main/java/com/fiek/todoapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.fiek.todoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
