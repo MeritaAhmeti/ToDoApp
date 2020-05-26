9d522ea1b649630aba2169735ce8867e62404fba
MeritaAhmeti <meritaahmeti1998@gmail.com>
2020-05-26 13:06:14 +0200

    Navigation drawer......in progress

--- app/src/main/java/com/fiek/todoapp/MainActivity.java
+++ app/src/main/java/com/fiek/todoapp/MainActivity.java
@@ -1,14 +1,94 @@
 package com.fiek.todoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+        private NavigationView navigationView;
+        private DrawerLayout drawer;
+        private View navHeader;
+
+        public static int navItemIndex = 0;
+
+        private static final String TAG_HOME = "home";
+        private static final String TAG_NOTIFICATIONS = "notifications";
+        private static final String TAG_SETTINGS = "settings";
+        public static String CURRENT_TAG = TAG_HOME;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.nav_activity);
+
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+
+        navHeader = navigationView.getHeaderView(0);
+
+        selectNavMenu();
+
+
+        setUpNavigationView();
+
+        if (savedInstanceState == null) {
+            navItemIndex = 0;
+            CURRENT_TAG = TAG_HOME;
+        }
+    }
+    private void selectNavMenu() {
+        navigationView.getMenu().getItem(navItemIndex).setChecked(true);
+    }
+
+    private void setUpNavigationView() {
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            // This method will trigger on item Click of navigation menu
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                //Check to see which item was being clicked and perform appropriate action
+                switch (menuItem.getItemId()) {
+                    //Replacing the main content with ContentFragment Which is our Inbox View;
+                    case R.id.nav_home:
+                        navItemIndex = 0;
+                        CURRENT_TAG = TAG_HOME;
+                        break;
+                    case R.id.nav_notifications:
+                        navItemIndex = 1;
+                        CURRENT_TAG = TAG_NOTIFICATIONS;
+                        break;
+                    case R.id.nav_settings:
+                        navItemIndex = 2;
+                        CURRENT_TAG = TAG_SETTINGS;
+                        break;
+                    case R.id.nav_about_us:
+
+                        drawer.closeDrawers();
+                        return true;
+                    default:
+                        navItemIndex = 0;
+                }
+
+                if (menuItem.isChecked()) {
+                    menuItem.setChecked(false);
+                } else {
+                    menuItem.setChecked(true);
+                }
+                menuItem.setChecked(true);
+
+
+
+                return true;
+            }
+        });
+
     }
 }
