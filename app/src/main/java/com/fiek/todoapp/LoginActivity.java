d3012ce23d2e34d46e5e45cc33bb8265e70dbe2e
MeritaAhmeti <meritaahmeti1998@gmail.com>
2020-06-05 15:01:14 +0200

    Remove ImageView from nav_header_main

---
+++ app/src/main/java/com/fiek/todoapp/LoginActivity.java
@@ -0,0 +1,68 @@
+package com.fiek.todoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText mTextUsername;
+    EditText mTextPassword;
+    Button mButtonLogin;
+    Button mButtonSignup;
+    DatabaseHelper DB;
+
+    ConstraintLayout loginbackground;
+    AnimationDrawable animationDrawable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        loginbackground = (ConstraintLayout) findViewById(R.id.loginbackground);
+        animationDrawable = (AnimationDrawable) loginbackground.getBackground();
+        animationDrawable.setEnterFadeDuration(4500);
+        animationDrawable.setExitFadeDuration(4500);
+        animationDrawable.start();
+
+
+        DB = new DatabaseHelper(this);
+        mTextUsername = (EditText)findViewById(R.id.edittext_username);
+        mTextPassword = (EditText)findViewById(R.id.edittext_password);
+        mButtonLogin = (Button)findViewById(R.id.button_login);
+        mButtonSignup = (Button) findViewById(R.id.btSignUp);
+        mButtonSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent  = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(registerIntent);
+
+            }
+        });
+        mButtonLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                String user = mTextUsername.getText().toString().trim();
+                String pwd = mTextPassword.getText().toString().trim();
+                Boolean res = DB.checkUser(user, pwd);
+                if (res == true) {
+                    Intent HomePage = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(HomePage);
+                }
+                else
+                {
+                    Toast.makeText(LoginActivity.this, "Login Error", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
