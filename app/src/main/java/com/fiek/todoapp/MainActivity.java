4d85e75a29ae326607691d931b2d61e788fa8010
MorinaElsa <morinaelsa01@gmail.com>
2020-06-17 01:55:48 +0200

    Working with Fragments in Navigation Drawer

--- app/src/main/java/com/fiek/todoapp/MainActivity.java
+++ app/src/main/java/com/fiek/todoapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.fiek.todoapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
@@ -14,10 +15,13 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -35,106 +39,148 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-        DatabaseReference reference;
-        RecyclerView newtodo;
-        ArrayList<MyToDo> list;
-        ToDoAdapter toDoAdapter;
-       // TextView titlepage;
+    private NavigationView navigationView;
+    private DrawerLayout drawer;
+    private View navHeader;
+    private Toolbar toolbar;
+    private FloatingActionButton fab;
+    private LinearLayout linearlayoutTodoBack;
 
 
 
-        private NavigationView navigationView;
-        private DrawerLayout drawer;
-        private Toolbar toolbar;
-        private View navHeader;
+    // index to identify current nav menu item
+    public static int navItemIndex = 0;
 
-        public static int navItemIndex = 0;
+    // tags used to attach the fragments
+    private static final String TAG_HOME = "home";
+    private static final String TAG_MAP = "map";
+    private static final String TAG_RATE = "rate";
+    public static String CURRENT_TAG = TAG_HOME;
 
-        private static final String TAG_HOME = "home";
-        private static final String TAG_NOTIFICATIONS = "notifications";
-        private static final String TAG_SETTINGS = "settings";
-        public static String CURRENT_TAG = TAG_HOME;
+    // toolbar titles respected to selected nav menu item
+    private String[] activityTitles;
 
-        private Handler mHandler;
+    // flag to load home fragment when user presses back key
+    private boolean shouldLoadHomeFragOnBackPress = true;
+    private Handler mHandler;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nav_activity);
-
-       // titlepage = findViewById(R.id.titlepage);
-        FloatingActionButton  btnAddNew = findViewById(R.id.btnaddnew);
-        btnAddNew.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent a = new Intent(MainActivity.this,AddTask.class);
-                startActivity(a);
-            }
-        });
-
-        //puna e te dhenave
-        newtodo= findViewById(R.id.newtodo);
-        newtodo.setLayoutManager(new LinearLayoutManager(this));
-        list = new ArrayList<MyToDo>();
-
-//        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
-//        newtodo.setLayoutManager(layoutManager);
-
-        //me marr te dhenat prej firebase
-        reference= FirebaseDatabase.getInstance().getReference().child("ToDoApp");
-        reference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                //kodi per rimarrjen e te dhenave  dhe zevendesimin e layout
-                for (DataSnapshot dataSnapshot1: dataSnapshot.getChildren())
-                {
-                    MyToDo p= dataSnapshot1.getValue(MyToDo.class);
-                    list.add(p);
-
-                }
-                toDoAdapter = new ToDoAdapter( MainActivity.this,list);
-                newtodo.setAdapter(toDoAdapter);
-                toDoAdapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                Toast.makeText(MainActivity.this, "No Data", Toast.LENGTH_SHORT).show();
-
-            }
-        });
-
-
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-
-
-
 
         mHandler = new Handler();
 
         drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         navigationView = (NavigationView) findViewById(R.id.nav_view);
+        fab = (FloatingActionButton) findViewById(R.id.btnaddnew);
+        linearlayoutTodoBack =(LinearLayout) findViewById(R.id.linearlayoutTodoBack);
 
+
+        // Navigation view header
         navHeader = navigationView.getHeaderView(0);
+        // load toolbar titles from string resources
+        activityTitles = getResources().getStringArray(R.array.nav_item_activity_titles);
 
-        selectNavMenu();
+
+        // initializing navigation menu
         setUpNavigationView();
 
         if (savedInstanceState == null) {
             navItemIndex = 0;
             CURRENT_TAG = TAG_HOME;
+            loadHomeFragment();
         }
     }
+
+
+    /***
+     * Returns respected fragment that user
+     * selected from navigation menu
+     */
+    private void loadHomeFragment() {
+        // selecting appropriate nav menu item
+        selectNavMenu();
+
+        // set toolbar title
+        setToolbarTitle();
+
+        // if user select the current navigation menu again, don't do anything
+        // just close the navigation drawer
+        if (getSupportFragmentManager().findFragmentByTag(CURRENT_TAG) != null) {
+            drawer.closeDrawers();
+
+            toggleFab();
+            toggle();
+            return;
+        }
+
+        // Sometimes, when fragment has huge data, screen seems hanging
+        // when switching between navigation menus
+        // So using runnable, the fragment is loaded with cross fade effect
+        // This effect can be seen in GMail app
+        Runnable mPendingRunnable = new Runnable() {
+            @Override
+            public void run() {
+                // update the main content by replacing fragments
+                Fragment fragment = getHomeFragment();
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.setCustomAnimations(android.R.anim.fade_in,
+                        android.R.anim.fade_out);
+                fragmentTransaction.replace(R.id.frame, fragment, CURRENT_TAG);
+                fragmentTransaction.commitAllowingStateLoss();
+            }
+        };
+
+        // If mPendingRunnable is not null, then add to the message queue
+        if (mPendingRunnable != null) {
+            mHandler.post(mPendingRunnable);
+        }
+
+
+        toggleFab();
+
+        toggle();
+        //Closing drawer on item click
+        drawer.closeDrawers();
+
+        // refresh toolbar menu
+        invalidateOptionsMenu();
+    }
+
+    private Fragment getHomeFragment() {
+        switch (navItemIndex) {
+            case 0:
+                // home
+                HomeFragment homeFragment = new HomeFragment();
+                return homeFragment;
+            case 1:
+                // photos
+                MapFragment mapFragment = new MapFragment();
+                return mapFragment;
+            case 2:
+                // photos
+                RateFragment rateFragment = new RateFragment();
+                return rateFragment;
+
+            default:
+                return new HomeFragment();
+        }
+    }
+
+    private void setToolbarTitle() {
+        getSupportActionBar().setTitle(activityTitles[navItemIndex]);
+    }
+
     private void selectNavMenu() {
         navigationView.getMenu().getItem(navItemIndex).setChecked(true);
     }
 
     private void setUpNavigationView() {
-
+        //Setting Navigation View Item Selected Listener to handle the item click of the navigation menu
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
 
             // This method will trigger on item Click of navigation menu
@@ -148,22 +194,25 @@ public class MainActivity extends AppCompatActivity {
                         navItemIndex = 0;
                         CURRENT_TAG = TAG_HOME;
                         break;
-                    case R.id.nav_notifications:
+                    case R.id.nav_map:
                         navItemIndex = 1;
-                        CURRENT_TAG = TAG_NOTIFICATIONS;
+                        CURRENT_TAG = TAG_MAP;
                         break;
-                    case R.id.nav_settings:
+                    case R.id.nav_rate:
                         navItemIndex = 2;
-                        CURRENT_TAG = TAG_SETTINGS;
+                        CURRENT_TAG = TAG_RATE;
                         break;
-                    case R.id.nav_about_us:
 
+                    case R.id.nav_about_us:
+                        // launch new intent instead of loading fragment
+                        startActivity(new Intent(MainActivity.this, AboutUsActivity.class));
                         drawer.closeDrawers();
                         return true;
                     default:
                         navItemIndex = 0;
                 }
 
+                //Checking if the item is in checked state or not, if not make it in checked state
                 if (menuItem.isChecked()) {
                     menuItem.setChecked(false);
                 } else {
@@ -171,23 +220,24 @@ public class MainActivity extends AppCompatActivity {
                 }
                 menuItem.setChecked(true);
 
-
+                loadHomeFragment();
 
                 return true;
             }
         });
 
+
         ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.openDrawer, R.string.closeDrawer) {
 
             @Override
             public void onDrawerClosed(View drawerView) {
-
+                // Code here will be triggered once the drawer closes as we dont want anything to happen so we leave this blank
                 super.onDrawerClosed(drawerView);
             }
 
             @Override
             public void onDrawerOpened(View drawerView) {
-
+                // Code here will be triggered once the drawer open as we dont want anything to happen so we leave this blank
                 super.onDrawerOpened(drawerView);
             }
         };
@@ -197,25 +247,41 @@ public class MainActivity extends AppCompatActivity {
 
         //calling sync state is necessary or else your hamburger icon wont show up
         actionBarDrawerToggle.syncState();
-
-
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.share_tab, menu);
-        return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.share:
-                Toast.makeText(this, "Share this app", Toast.LENGTH_SHORT).show();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawers();
+            return;
         }
+
+        // This code loads home fragment when back key is pressed
+        // when user is in other fragment than home
+        if (shouldLoadHomeFragOnBackPress) {
+            // checking if user is on other navigation menu
+            // rather than home
+            if (navItemIndex != 0) {
+                navItemIndex = 0;
+                CURRENT_TAG = TAG_HOME;
+                loadHomeFragment();
+                return;
+            }
+        }
+
+        super.onBackPressed();
+    }
+    private void toggleFab() {
+        if (navItemIndex == 0)
+            fab.show();
+        else
+            fab.hide();
+    }
+    private void toggle(){
+        if(navItemIndex != 0)
+
+            linearlayoutTodoBack.setVisibility(View.INVISIBLE);
     }
 
-    }
 
+}
