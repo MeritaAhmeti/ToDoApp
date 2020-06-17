da5fc749a3b16b5b5dc74d44c7dc2ec4fce173d1
MeritaAhmeti <meritaahmeti1998@gmail.com>
2020-06-18 00:56:45 +0200

    JSON Parsing

--- app/src/main/java/com/fiek/todoapp/AboutUsActivity.java
+++ app/src/main/java/com/fiek/todoapp/AboutUsActivity.java
@@ -3,12 +3,19 @@ package com.fiek.todoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class AboutUsActivity extends AppCompatActivity {
+   public static TextView data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+
+        data = (TextView)findViewById(R.id.fetcheddata);
+
+        fetchData process = new fetchData();
+        process.execute();
     }
 }
