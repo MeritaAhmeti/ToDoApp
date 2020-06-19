ae308431aa3f27014dfbdb1fa9912a99daf53e32
MorinaElsa <morinaelsa01@gmail.com>
2020-06-19 19:46:06 +0200

    Shared Preferences

--- app/src/main/java/com/fiek/todoapp/LoginActivity.java
+++ app/src/main/java/com/fiek/todoapp/LoginActivity.java
@@ -2,22 +2,33 @@ package com.fiek.todoapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
     EditText mTextUsername;
     EditText mTextPassword;
     Button mButtonLogin;
     Button mButtonSignup;
+    CheckBox mcheckBox;
     DatabaseHelper DB;
+
+    private SharedPreferences mPreferences;
+    private  SharedPreferences.Editor mEditor;
 
     ConstraintLayout loginbackground;
     AnimationDrawable animationDrawable;
@@ -38,12 +49,24 @@ public class LoginActivity extends AppCompatActivity {
         mTextPassword = (EditText)findViewById(R.id.edittext_password);
         mButtonLogin = (Button)findViewById(R.id.button_login);
         mButtonSignup = (Button) findViewById(R.id.btSignUp);
+        mcheckBox = (CheckBox) findViewById(R.id.checkBox);
+
+        //SharedPreferences
+        mPreferences= PreferenceManager.getDefaultSharedPreferences(this);
+        mPreferences=getSharedPreferences("com.fiek.todoapp", Context.MODE_PRIVATE);
+        mEditor=mPreferences.edit();
+
+        checkSharedPreferences();
+
+
+
+
+
         mButtonSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent registerIntent  = new Intent(LoginActivity.this,RegisterActivity.class);
                 startActivity(registerIntent);
-
             }
         });
         mButtonLogin.setOnClickListener(new View.OnClickListener(){
@@ -52,12 +75,38 @@ public class LoginActivity extends AppCompatActivity {
                 String user = mTextUsername.getText().toString().trim();
                 String pwd = mTextPassword.getText().toString().trim();
                 Boolean res = DB.checkUser(user, pwd);
-                if (res == true) {
+                if (res == true ) {
+
                     Intent HomePage = new Intent(LoginActivity.this, MainActivity.class);
                     startActivity(HomePage);
-                }
-                else
-                {
+                    if (mcheckBox.isChecked()) {
+
+                        mEditor.putString(getString(R.string.checkbox), "True");
+                        mEditor.commit();
+
+                        String username = user;
+                        mEditor.putString(getString(R.string.username), username);
+                        mEditor.commit();
+
+                        String password = pwd;
+                        mEditor.putString(getString(R.string.password), password);
+                        mEditor.commit();
+                    }else{
+                        mEditor.putString(getString(R.string.checkbox), "False");
+                        mEditor.commit();
+
+                        String username = user;
+                        mEditor.putString(getString(R.string.username), "");
+                        mEditor.commit();
+
+                        String password = pwd;
+                        mEditor.putString(getString(R.string.password), "");
+                        mEditor.commit();
+                    }
+
+                }else {
+
+
                     Toast.makeText(LoginActivity.this, "Login Error", Toast.LENGTH_SHORT).show();
                 }
             }
