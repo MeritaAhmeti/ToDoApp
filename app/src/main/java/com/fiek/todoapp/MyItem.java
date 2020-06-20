97ca413a6e2946128ab42c04fb8fdca2833241d8
MorinaElsa <morinaelsa01@gmail.com>
2020-06-20 22:47:57 +0200

    Cluster Map

--- app/src/main/java/com/fiek/todoapp/MyItem.java
+++ app/src/main/java/com/fiek/todoapp/MyItem.java
@@ -1,8 +1,44 @@
 package com.fiek.todoapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
+import com.google.android.gms.maps.model.LatLng;
+import com.google.maps.android.clustering.ClusterItem;
 
-public class MyItem  {
+public class MyItem implements ClusterItem {
+    private LatLng position;
+    private String title;
+    private String snippet;
 
+    public MyItem(LatLng position) {
+        this.position = position;
+    }
+
+    public MyItem(LatLng position, String title, String snippet) {
+        this.position = position;
+        this.title = title;
+        this.snippet = snippet;
+    }
+
+    @NonNull
+    @Override
+    public LatLng getPosition() {
+        return position;
+    }
+
+    @Nullable
+    @Override
+    public String getTitle() {
+        return title;
+    }
+
+    @Nullable
+    @Override
+    public String getSnippet() {
+        return snippet;
+    }
 }
+
+
 
