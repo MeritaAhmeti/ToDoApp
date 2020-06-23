3bf9e5b61e772542be6848c73afd618992390d3c
MeritaAhmeti <meritaahmeti1998@gmail.com>
2020-06-23 18:16:58 +0200

    Adding reminder...

--- app/src/main/java/com/fiek/todoapp/AddTask.java
+++ app/src/main/java/com/fiek/todoapp/AddTask.java
@@ -1,31 +1,38 @@
 package com.fiek.todoapp;
 
+import com.allyants.notifyme.NotifyMe;
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
+import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
 import android.content.Intent;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
+import java.util.Calendar;
 import java.util.Random;
 
-public class AddTask extends AppCompatActivity {
+
+public class AddTask extends AppCompatActivity  implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener{
 
     TextView titlepage, addtitle, adddesc, adddate;
     EditText titletodo, desctodo, datetodo;
-    Button btnSaveTask, btnCancel;
+    Button btnSaveTask, btnCancel,notify;
     DatabaseReference reference;
     Integer todoNum = new Random().nextInt();
     String keytodo = Integer.toString(todoNum);
+    Calendar now = Calendar.getInstance();
+    TimePickerDialog tpd;
+    DatePickerDialog dpd;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,13 +51,34 @@ public class AddTask extends AppCompatActivity {
 
         btnSaveTask = findViewById(R.id.btnSaveTask);
         btnCancel = findViewById(R.id.btnCancel);
+        notify = findViewById(R.id.notify);
+
+
+        dpd = DatePickerDialog.newInstance(
+                AddTask.this,
+                now.get(Calendar.YEAR),
+                now.get(Calendar.MONTH),
+                now.get(Calendar.DAY_OF_MONTH)
+
+        );
+
+        tpd = TimePickerDialog.newInstance(
+                AddTask.this,
+                now.get(Calendar.HOUR_OF_DAY),
+                now.get(Calendar.MINUTE),
+                now.get(Calendar.SECOND),
+                false
+        );
+
+
 
         btnSaveTask.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
+            @Override
+            public void onClick(View v) {
                 // insert data to databasea
                 reference = FirebaseDatabase.getInstance().getReference().child("ToDoApp").
                         child("Todo" + todoNum);
+
                 reference.addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
@@ -59,26 +87,96 @@ public class AddTask extends AppCompatActivity {
                         dataSnapshot.getRef().child("desctodo").setValue(desctodo.getText().toString());
                         dataSnapshot.getRef().child("datetodo").setValue(datetodo.getText().toString());
                         dataSnapshot.getRef().child("keytodo").setValue(keytodo);
-
                         Intent a = new Intent(AddTask.this,MainActivity.class);
                         startActivity(a);
-
                     }
+
 
                     @Override
                     public void onCancelled(DatabaseError databaseError) {
-
+                        Toast.makeText(AddTask.this, "No Data", Toast.LENGTH_SHORT).show();
                     }
+
                 });
+
+
             }
 
+
+
         });
-          btnCancel.setOnClickListener(new View.OnClickListener() {
-              @Override
-              public void onClick(View v) {
-                  Intent a = new Intent(AddTask.this,MainActivity.class);
-                  startActivity(a);
-              }
-          });
+        notify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dpd.show(getSupportFragmentManager(), "Datepickerdialog");
+                reference = FirebaseDatabase.getInstance().getReference().child("ToDoApp").
+                        child("Todo" + todoNum);
+
+                reference.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+
+                        dataSnapshot.getRef().child("titletodo").setValue(titletodo.getText().toString());
+                        dataSnapshot.getRef().child("desctodo").setValue(desctodo.getText().toString());
+                        dataSnapshot.getRef().child("datetodo").setValue(datetodo.getText().toString());
+                        dataSnapshot.getRef().child("keytodo").setValue(keytodo);
+                    }
+
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        Toast.makeText(AddTask.this, "No Data", Toast.LENGTH_SHORT).show();
+                    }
+
+                });
+            }
+        });
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a = new Intent(AddTask.this,MainActivity.class);
+                startActivity(a);
+            }
+        });
+
+    }
+
+
+
+    @Override
+    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+        now.set(Calendar.YEAR,year);
+        now.set(Calendar.MONTH,monthOfYear);
+        now.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+        tpd.show(getSupportFragmentManager(), "Timepickerdialog");
+
+
+    }
+
+    @Override
+    public void onTimeSet(TimePickerDialog view, int hourOfDay, int minute, int second) {
+        now.set(Calendar.HOUR_OF_DAY,hourOfDay);
+        now.set(Calendar.MINUTE,minute);
+        now.set(Calendar.SECOND,second);
+        Intent intent = new Intent(getApplicationContext(),SnoozingActivity.class);
+        intent.putExtra("test","I am a String");
+
+        NotifyMe notifyMe = new NotifyMe.Builder(getApplicationContext())
+                .title(titletodo.getText().toString())
+                .content(desctodo.getText().toString())
+                .color(255,0,0,255)
+                .led_color(255,255,255,255)
+                .time(now)
+                .addAction(intent,"Snooze",false)
+                .key("test")
+                .addAction(new Intent(),"Dismiss",true,false)
+                .addAction(intent,"Done")
+                .large_icon(R.mipmap.ic_launcher_round)
+                .rrule("FREQ=MINUTELY;INTERVAL=5;COUNT=2")
+                .build();
+        Intent a = new Intent(AddTask.this,MainActivity.class);
+        startActivity(a);
+
+
     }
 }
